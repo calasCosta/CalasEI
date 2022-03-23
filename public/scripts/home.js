144fd5799c27049dc018ec95de4316ac8431f4b3
Noami Costa <noamicafft@gmail.com>
2022-03-23 20:14:57 +0000

    full commit till now

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const connection = require("./db/db");
 
+/**
+ * Route method get to render the main page
+ */
 router.get("/", function (req, res) {
 
     connection.query("select * from categories", function (err, rows) {
@@ -10,6 +13,10 @@ router.get("/", function (req, res) {
     });
 });
 
+
+/**
+ * Route method post to create the categories
+ */
 router.post("/createCategory", function (req, res) {
     let category = req.body.txtCategory;
 
@@ -32,49 +39,101 @@ router.post("/createCategory", function (req, res) {
 });
 
 let url ="";
-
+let contentId = 0;
+/**
+ * Route method get to render the contents page
+ */
 router.get("/category/:categoryId", function (req, res) {
-    
-    let selectCategory = `select * from categories where category_id = ${req.params.categoryId}`;
-    connection.query(selectCategory, function (err, result) {
+
+    let selectCategory = `select category from categories where category_id = ${req.params.categoryId}`;
+    connection.query(selectCategory, function (err, resultCategory) {
         if(err){throw err;}
 
         let selectContents = `select * from contents where category_id = ${req.params.categoryId}`;
         connection.query(selectContents, function (error, rows){
             if(error){throw error;}
 
-            if(rows.length){
-                if(!!url === false){
-                    url = rows[0].videoUrl;
-                }
-            }else{
-                url =""
-            }
+            updateVideosUrl(rows);
 
-            res.render("category", {
-                category:result[0].category,
-                categoryId: req.params.categoryId,
-                contents : rows,
-                urlContent : url  }
-            );
+            let selectNotes  = `select * from notes where content_id= ${contentId}`;
+            connection.query(selectNotes, function(error, notesResult){
+                if(error){throw error;}
+
+                console.log(notesResult)
+
+                res.render("category", {
+                    category:resultCategory[0].category,
+                    categoryId: req.params.categoryId,
+                    contents : rows,
+                    urlContent : url,
+                    contentId: contentId,
+                    notes: notesResult}
+                );
+            });
+
         });
     });
 });
 
+/**
+ * Method to update the videoUrl. It receive an array of contents and then
+ * 1- checks if its empty - if true reset url
+ * 2 - else, it checks if url has a false boolean value, if true
+ *     2.1 - it checks if this url belongs this category, if not it set url width the value of first element
+ * 3 - else set the url and content id with the values of first element in array
+ * @param {*} rows 
+ */
+ function updateVideosUrl(rows){
+    if(!rows.length){
+        url = ""; 
+        contentId = 0;
+    }else{
 
-router.post("/category/:categoryId/content/:contentId", (req, res)=>{
+        let belongsToThisCategory = (function(){
+            let belongsToThisCategory = false;
+            for(let content of rows){
+               if(url === content.videoUrl){
+                    belongsToThisCategory = true;
+               }
+            }
+            return belongsToThisCategory;
+        }())
 
-    let selectContent = "select * from contents where content_id = "+req.params.contentId;
-    connection.query(selectContent, function (err, result) {
-        if(err){throw err;}
+        if((!!url && !belongsToThisCategory) || !!url === false){
+            url = rows[0].videoUrl;
+            contentId = rows[0].content_id;
+        }
+    }
+}
 
-        url = result[0].videoUrl;
-    })
-    console.log("asdfdsfasdfas " + req.params.contentId)
-    res.redirect("back")
-});
+/* before refactoring
+function updateVideosUrl(rows){
+    if(!rows.length){
+        url = ""; 
+        contentId = 0;
+    }else{
+        if(!!url){
+            let belongsToThisCategory = false;
+            for(let content of rows){
+               if(url === content.videoUrl){
+                    belongsToThisCategory = true;
+               }
+            }
 
+            if(!belongsToThisCategory){
+                url = rows[0].videoUrl;
+            }
+        }else{
+            url = rows[0].videoUrl;
+            contentId = rows[0].content_id;
+        }
+    }
+}
+*/
 
+/**
+ * Route method POST to create a new content
+ */
 router.post("/category/:categoryId/createContent", function (req, res) {
     let content = req.body.txtContent;
     let urlContent = req.body.txtUrlContent;
@@ -89,5 +148,48 @@ router.post("/category/:categoryId/createContent", function (req, res) {
     res.redirect("back");
 });
 
+/**
+ * Route method POST to select a content on sidebar
+ */
+router.post("/category/:categoryId/content/:contentId", (req, res)=>{
+
+    let selectContent = "select * from contents where content_id = "+req.params.contentId;
+    connection.query(selectContent, function (err, result) {
+        if(err){throw err;}
+
+        url = result[0].videoUrl;
+        contentId = req.params.contentId;
+    });
+    console.log("content:  " + req.params.contentId)
+    res.redirect("back")
+});
+
+/**
+ * Route method POST to create notes for a specific content
+ */
+router.post("/category/:categoryId/content/:contentId/takeNote", (req, res)=>{
+
+    let expression = req.body.expression;
+    let meaning = req.body.meaning;
+    let example = req.body.example;
+    let pronunciation = req.body.pronunciation;
+
+    if(!!expression){
+        let values = [[expression, meaning, example, pronunciation, req.params.contentId]]
+        let createNote = `insert into notes (expression, meaning, example, pronunciation, content_id) values ?`;
+
+        
+
+        connection.query(createNote, [values], function (err, result) {
+            if(err){throw err;}
+
+            console.log("content fk:  " + req.params.contentId)
+            console.log("expression:  " + expression + "taken successfully")
+            
+        });
+    }
+
+    res.redirect("back")
+});
 
 module.exports = router;
